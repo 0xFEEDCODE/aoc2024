db8f82bc85ad4fe47573380971280e5625b16294
some00000001 <samo.seman96@gmail.com>
2024-11-30 18:40:28 +0100

    c

---
+++ aoc2024/day01/day01.fs
@@ -0,0 +1,13 @@
+﻿module aoc2024.day01
+open helper
+
+
+let solve() =
+    let inp = helper.getInput()
+    
+    
+    //helper.submitAnswer 1 ans1
+    
+    //helper.submitAnswer 2 ans1
+    
+    0
