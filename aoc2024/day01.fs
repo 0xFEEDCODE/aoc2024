a2a0ab7c95c2b07f08b87021b698064689f30648
some00000001 <samo.seman96@gmail.com>
2024-12-01 07:01:25 +0100

    c

---
+++ aoc2024/day01.fs
@@ -0,0 +1,30 @@
+﻿module aoc2024.day01
+
+open aoc2024.helper
+
+let solve () =
+    let mutable l1 = List.empty
+    let mutable l2 = List.empty
+
+    util.getInput ()
+    |> Seq.iter (fun x ->
+        let spl = (x.Split "   ")
+        l1 <- l1 @ [ int spl[0] ]
+        l2 <- l2 @ [ int spl[1] ])
+
+    l1 <- List.sortDescending l1 |> Seq.rev |> Seq.toList
+    l2 <- List.sortDescending l2 |> Seq.rev |> Seq.toList
+
+    let z = l1 |> Seq.zip l2
+
+    let ans1 = z |> Seq.sumBy (fun x -> abs (fst x - snd x))
+
+    let ans2 =
+        l2 |> Seq.sumBy (fun x -> x * (l1 |> Seq.where (fun y -> x = y) |> Seq.length))
+
+    (*
+    util.submitAnswer 1 ans1
+    util.submitAnswer 2 ans2
+    *)
+
+    0
---
+++ aoc2024/util.fs
@@ -0,0 +1,139 @@
+module aoc2024.helper
+
+open System
+open System.Collections.Generic
+open System.Diagnostics
+open System.IO
+open System.Reflection
+open System.Text.RegularExpressions
+open Microsoft.Extensions.Configuration
+open System.Net.Http
+
+open System.Threading
+
+type Point2D =
+    struct
+        val x: int
+        val y: int
+
+        member this.GetManhattanDistance(p: Point2D) = abs (this.x - p.x) + abs (this.y - p.y)
+
+    end
+
+type Point3D =
+    struct
+        val x: int
+        val y: int
+        val z: int
+    end
+
+type Util() =
+    let httpClient = new HttpClient()
+
+    let sessionToken =
+        let secrets =
+            ConfigurationBuilder().AddUserSecrets(Assembly.GetExecutingAssembly()).Build()
+
+        (secrets.AsEnumerable() |> Seq.find (fun s -> s.Key = "tokenAOC")).Value
+
+    let getCallerModuleName () =
+        let st = StackTrace()
+        let mutable i = 0
+
+        while (st.GetFrame(i).GetMethod().ReflectedType.Name = "Util") do
+            i <- i + 1
+
+        st.GetFrame(i).GetMethod().ReflectedType.Name
+
+    let getPathToInputFile moduleName =
+        Path.Combine(Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName, $"{moduleName}.txt")
+
+    member this.readInput() =
+        let moduleName = getCallerModuleName ()
+        File.ReadLines(getPathToInputFile moduleName)
+
+    member this.getInput(?overrideIfExists: bool) =
+        let overrideIfExists = defaultArg overrideIfExists false
+
+        let moduleName = getCallerModuleName ()
+        let pathToInputFile = getPathToInputFile moduleName
+
+        let mutable shouldDownloadInput = true
+
+        if (not overrideIfExists) then
+            if (File.Exists pathToInputFile) then
+                shouldDownloadInput <- false
+
+        if shouldDownloadInput then
+            printfn "Downloading"
+
+            let urlDayPart =
+                if moduleName.StartsWith "day0" then
+                    moduleName[4..]
+                else
+                    moduleName[3..]
+
+            let url = $"https://adventofcode.com/2024/day/{urlDayPart}/input"
+            let request = new HttpRequestMessage(HttpMethod.Get, url)
+            request.Headers.Add("Cookie", $"session={sessionToken}")
+            let cts = new CancellationTokenSource()
+            let response = httpClient.Send request
+
+            if (not response.IsSuccessStatusCode) then
+                failwith $"Could not get input, http error - {response.StatusCode}"
+
+            let content = response.Content.ReadAsStringAsync(cts.Token)
+            content |> Async.AwaitTask |> ignore
+            File.WriteAllText(pathToInputFile, content.Result)
+
+        this.readInput ()
+
+    member this.submitAnswer part answer =
+        let moduleName = getCallerModuleName ()
+
+        let urlDayPart =
+            if moduleName.StartsWith "day0" then
+                moduleName[4..]
+            else
+                moduleName[3..]
+
+        let url = $"https://adventofcode.com/2024/day/{urlDayPart}/answer"
+        let request = new HttpRequestMessage(HttpMethod.Post, url)
+        request.Headers.Add("Cookie", $"session={sessionToken}")
+
+        let payload = List<KeyValuePair<string, string>>()
+        payload.Add(KeyValuePair<string, string>("level", part.ToString()))
+        payload.Add(KeyValuePair<string, string>("answer", answer.ToString()))
+        request.Content <- new FormUrlEncodedContent(payload)
+
+        let cts = new CancellationTokenSource()
+        let response = httpClient.Send request
+
+        if (not response.IsSuccessStatusCode) then
+            failwith $"Could submit answer, http error - {response.StatusCode}"
+
+        let content = response.Content.ReadAsStringAsync(cts.Token)
+        content |> Async.AwaitTask |> ignore
+
+        let html = content.Result
+        let pattern = "(?i)(?s)<article><p>(?<content>.*?)</p></article>"
+        let m = Regex.Match(html, pattern)
+
+        if m.Success then
+            let content = m.Groups.["content"].Value
+            printfn $"%A{content}"
+            content
+        else
+            failwith "wtf"
+
+    member this.createGrid<'a> nRows nCols =
+        Array.zeroCreate<Array> nRows
+        |> Seq.map (fun _ -> Array.zeroCreate<'a> nCols)
+        |> Seq.toArray
+
+    member this.createGridDV<'a> nRows nCols (initValue: 'a) =
+        Array.zeroCreate<Array> nRows
+        |> Seq.map (fun _ -> Array.create<'a> nCols initValue)
+        |> Seq.toArray
+
+let util = Util()
