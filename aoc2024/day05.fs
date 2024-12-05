64154eec1ea498863f55a35c569c6888ed051a25
sm <smsm@demant.com>
2024-12-05 09:52:38 +0100

    c

--- aoc2024/day05.fs
+++ aoc2024/day05.fs
@@ -3,87 +3,38 @@
 open System.Collections.Generic
 open aoc2024.util
 
+let compareByOrder (orderTable: Dictionary<'a, List<'a>>) a b =
+    if (orderTable.ContainsKey a && orderTable[a].Contains(b)) then -1 else 1
+
 let solve() =
     let io = aocIO
     let inp = io.getInput()
     
-    let mutable nums = Dictionary<int, List<int>>()
+    let mutable orderTable = Dictionary<int, List<int>>()
     
-    (*
-    let rec isLower n1 n2 =
-        if nums[n1].Contains n2 then
-            true
-        else
-            isLower nums[]
-            *)
+    let orderComparer = compareByOrder orderTable
     
-    let pages = List<string>()
-    for l in inp do
-        if l.Contains "|" then
-            let spl = l.Split "|"
-            let n1 = spl[0] |> int
-            let n2 = spl[1] |> int
-            if not(nums.ContainsKey(n1)) then
-                nums[n1] <- List<int>()
-            if not(nums.ContainsKey(n2)) then
-                nums[n2] <- List<int>()
-            nums[n1].Add n2
-        else if l.Contains "," then
-            pages.Add l
+    let pages = List<int array>()
+    
+    inp |> Seq.iter(fun line ->
+        if line.Contains "|" then
+            let nums = line |> String.extractAllNums
+            let n1, n2 = (nums[0], nums[1])
+            nums |> Seq.iter(fun n -> if not(orderTable.ContainsKey(n)) then orderTable[n] <- List<int>())
+            orderTable[n1].Add n2
+        else if line.Contains "," then
+            pages.Add (line |> String.extractAllNums))
                 
-                
-    (*
-    for num in nums do
-        for otherNums in nums do
-            if num.Key <> otherNums.Key then
-                if otherNums.Value.Contains num.Key then
-                    num.Value.AddRange num.Value
-                    *)
+    let ans1, ans2 =
+            pages |> Seq.fold(fun (ans1, ans2) page ->
+                let isSorted = page |> Seq.pairwise |> Seq.forall(fun (n1, n2) -> (orderComparer n1 n2) = -1)
+                if isSorted then
+                    (ans1 + page[(page |> Seq.length)/2], ans2)
+                else
+                    let sorted = page |> Seq.sortWith(orderComparer) |> Seq.toArray
+                    (ans1, ans2 + sorted[(sorted |> Seq.length)/2]))
+                (0, 0)
     
-    (*
-    let mutable s = 0
-    for p in pages do
-        let ns = p.Split "," |> Seq.map int |> Seq.toArray
-        let mutable v = true
-        for i in 0..(ns |> Seq.length)-2 do
-            let n1 = nums[ns[i]]
-            let n2 = ns[i+1]
-            if not (n1.Contains(n2)) then
-                v <- false
-        if v then
-            let mv = ns[(ns.Length/2)]
-            &s += mv
-            printfn $"%A{(v, p, mv)}"
-            *)
-    let mutable s = 0
-    for p in pages do
-        let ns = p.Split "," |> Seq.map int |> Seq.toArray
-        let mutable v = true
-        for i in 0..(ns |> Seq.length)-2 do
-            let n1 = nums[ns[i]]
-            let n2 = ns[i+1]
-            if not (n1.Contains(n2)) then
-                v <- false
-        if v then
-            let mv = ns[(ns.Length/2)]
-            //&s += mv
-            printfn $""
-            //printfn $"%A{(v, p, mv)}"
-        else
-            let nl = List<int>()
-            for i in 0..(ns |> Seq.length)-1 do
-                let f = ns[i]
-                if not (nl.Contains(ns[i])) then
-                    if(nl |> Seq.length |> (=) 0) then
-                        nl.Insert (0, f)
-                    else
-                        let idxf = nl.FindIndex(fun x -> nums[x].Contains f)
-                        nl.Insert ((if idxf <> -1 then idxf else (nl.Count)), f)
-            &s += nl[(ns.Length/2)]
-                
-    
-    
-    printfn $"%A{s}"
-    //io.submitAnswer 1 ans1
-    //io.submitAnswer 2 s
+    printfn $"%A{ans1}"
+    printfn $"%A{ans2}"
     0
