4983306095f2337464c4eddb185da4524c2873ba
some00000001 <samo.seman96@gmail.com>
2024-12-07 17:18:04 +0100

    c

--- aoc2024/day07.fs
+++ aoc2024/day07.fs
@@ -1,8 +1,9 @@
 ﻿module aoc2024.day07
+open System
 open aoc2024.util
 
-let isValid (target: bigint) (ns: bigint array) operators =
-    let rec loop (acc: bigint) (ns: bigint array) =
+let isValid target (ns: int64 array) operators =
+    let rec loop acc (ns: int64 array) =
         if (ns |> Seq.length) = 0 then
             acc = target
         else
@@ -14,17 +15,17 @@ let solve() =
     let inp = io.getInput()
     
     let operatorsPt1 = [(+); (*);]
-    let operatorsPt2 = [(+); (*); (fun a b -> bigint.Parse(a.ToString()+b.ToString()))]
+    let operatorsPt2 = [(+); (*); (fun a b -> Int64.Parse(a.ToString()+b.ToString()))]
     
-    let execute operators =
+    let calculate operators =
         inp |> Seq.map(fun line ->
-            let nums = line |> String.extractAllNumsU |> Seq.map bigint |> Seq.toArray
+            let nums = line |> String.extractAllNumsU |> Seq.map int64 |> Seq.toArray
             (nums[0], nums[1..])
         )
         |> Seq.where(fun (target, nums) -> isValid target nums operators)
         |> Seq.sumBy fst
     
-    let a1, a2 = (execute operatorsPt1, execute operatorsPt2)
+    let a1, a2 = (calculate operatorsPt1, calculate operatorsPt2)
         
     printfn $"%A{a1}"
     printfn $"%A{a2}"
