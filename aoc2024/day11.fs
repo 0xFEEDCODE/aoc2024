399b6349622a06a417888587ddd79b8d56fb78b1
some00000001 <samo.seman96@gmail.com>
2024-12-13 05:46:00 +0100

    c

--- aoc2024/day11.fs
+++ aoc2024/day11.fs
@@ -4,25 +4,6 @@ open System
 open System.Collections.Generic
 open aoc2024.util
 
-type Spl<'T> = { L: 'T; R: 'T }
-
-type Link =
-    { V: uint64
-      mutable Left: Option<Link>
-      mutable Right: Option<Link> }
-
-
-type Link with
-    static member Create(v: uint64) = { V = v; Left = None; Right = None }
-    member this.Assignleft(n: Link) = this.Left <- Some(n)
-    member this.AssignRight(n: Link) = this.Right <- Some(n)
-
-type Entry =
-    | Split of Spl<string>
-    | Splits of Spl<string> list
-    | Zero
-    | Odd of uint64
-    | Odds of uint64 list
 
 let nDigits n = (int (log10 (n |> double)) + 1)
 
@@ -44,63 +25,47 @@ let solve () =
     let mutable stones =
         List(io.getInput () |> Seq.head |> String.extractAllNumsUint64 |> Seq.toList)
 
-    let mutable lnk = new Dictionary<uint64, Link>()
-    lnk[0UL] <- Link.Create(0UL)
     let nxt = Dictionary<uint64, uint64 * uint64 option>()
 
-    let st = Stack()
-    stones |> Seq.iter st.Push
-    st.Push(336509UL)
-    st.Push(138944UL)
+    let fnd n r =
+        let q = Queue()
+        q.Enqueue(n)
 
-    let fnd (st: Stack<uint64>) =
-        let mutable nl = List<List<uint64>>()
+        let nStElements = q.Count
 
-        for i in 0..30 do
-            let mutable ll = List<uint64>()
-
-            while (st.Count > 0) do
-                match st.Pop() with
+        for _ in 0..r do
+            for _ in 0..nStElements do
+                match q.Dequeue() with
                 | n when nxt.ContainsKey n ->
-                    let (f, s) = nxt[n]
-                    ll.Add(f)
+                    let f, s = nxt[n]
+                    q.Enqueue(f)
 
                     if s.IsSome then
-                        ll.Add s.Value
+                        q.Enqueue(s.Value)
                 | 0UL ->
                     nxt.Add(0UL, (1UL, None))
-                    ll.Add(1UL)
+                    q.Enqueue(1UL)
                 | n when isEven n ->
                     let nd = nDigits n
                     let l, r = split n nd
                     nxt.Add(n, (l, Some(r)))
-                    ll.Add(l)
-                    ll.Add(r)
+                    q.Enqueue(l)
+                    q.Enqueue(r)
                 | n ->
                     let nv = n * 2024UL
                     nxt.Add(n, (nv, None))
-                    ll.Add(nv)
-
-            nl.Add(ll)
-
-            for x in ll do
-                st.Push x
-
-    fnd st
+                    q.Enqueue(nv)
 
     let m = Dictionary<uint64, Dictionary<int, uint64>>()
 
     let nr n targetR =
-        let targetR = targetR + 1
 
         let rec loop n rl sr acc =
             if (rl = 0) then
                 acc
             else
                 if not (nxt.ContainsKey(n)) then
-                    let st = Stack()
-                    st.Push(n)
-                    fnd st
+                    fnd n rl
 
                 let f, s = nxt[n]
                 let nsr = (rl - 1)
@@ -135,12 +100,5 @@ let solve () =
 
         loop n targetR targetR 0UL
 
-    let res = stones |> Seq.fold (fun acc x -> acc + (nr x 74)) 0UL
+    let res = stones |> Seq.fold (fun acc x -> acc + (nr x 2500)) 0UL
     printfn $"%A{res}"
-
-    (*
-    for i in 0..75 do
-        let r = nr 0UL i
-        printfn $"%A{(i, r)}"
-        *)
-    ()
---
+++ aoc2024/day13.fs
@@ -0,0 +1,11 @@
+﻿module aoc2024.day13
+
+open aoc2024.util
+
+let solve () =
+    let io = aocIO
+    let inp = io.getInput ()
+
+    //io.submitAnswer 1 a1
+    //io.submitAnswer 2 a2
+    0
