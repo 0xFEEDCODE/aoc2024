19d7783a1bb02b7434db53ca22b7bbe127fa0ade
some00000001 <samo.seman96@gmail.com>
2024-12-16 22:08:52 +0100

    c

--- aoc2024/day16.fs
+++ aoc2024/day16.fs
@@ -4,29 +4,21 @@ open System.Collections.Generic
 open Microsoft.FSharp.Core
 open aoc2024.util
 
-(*type R =
-    { X: int
-      Y: int }
-
-    static member C(x: int, y: int) = { X = x; Y = y }
-    
-type C(x: int, y: int) =
-    let mutable X = x
-    let mutable Y = y
-    
-    *)
-
-
 type Dir =
     | L
     | R
     | U
     | D
 
-let solve () =
-    let mutable a1 = bigint.Zero
-    let mutable a2 = bigint.Zero
+type Record =
+    { cp: Point2D
+      cd: Dir
+      score: int
+      path: Point2D list }
 
+    member this.Unpack = (this.cp, this.cd, this.score, this.path)
+
+let solve () =
     let canMove d1 d2 =
         match d1 with
         | L -> d2 = L || d2 = U || d2 = D
@@ -40,13 +32,6 @@ let solve () =
             .Add(Point2D(0, -1), Dir.U)
             .Add(Point2D(-1, 0), Dir.L)
             .Add(Point2D(1, 0), Dir.R)
-
-    let dirs =
-        Map.empty
-            .Add(Dir.D, Point2D(0, 1))
-            .Add(Dir.U, Point2D(0, -1))
-            .Add(Dir.L, Point2D(-1, 0))
-            .Add(Dir.R, Point2D(1, 0))
 
     let io = aocIO
     let inp = io.getInput ()
@@ -68,115 +53,70 @@ let solve () =
 
             ()
 
-    let noffs = [ Point2D(-1, 0); Point2D(1, 0); Point2D(0, -1); Point2D(0, 1) ]
+    let neighbourOffsets =
+        [ Point2D(-1, 0); Point2D(1, 0); Point2D(0, -1); Point2D(0, 1) ]
 
-    let mutable visited = Dictionary<Point2D, int>()
+    let mutable visitedBestPath = Dictionary<Point2D, int>()
 
-    let pr () =
-        for y in 0..rows do
-            for x in 0..cols do
-                if (visited.ContainsKey(Point2D(x, y))) then
-                    printf "O"
-                else
-                    printf $"%c{gr[y][x]}"
+    let fp (start: Point2D) (target: Point2D) (sp: int) =
 
-            printfn ""
+        let mutable visited = Dictionary<Point2D * Dir, int>()
 
-    let mutable mc = None
+        let mutable q = PriorityQueue()
 
-    let fp (start: Point2D) (target: Point2D) (sp: int) (sdir: Dir) (mv: Option<int>) =
-
-        let mutable cameFrom = Dictionary<Point2D * Dir, Option<Point2D * Dir>>()
-
-        cameFrom.Add((start, sdir), None)
-
-        let mutable q = PriorityQueue<(Point2D * Dir) * ((Point2D * int) * Dir) list, int>()
-        q.Enqueue(((start, sdir), []), sp)
-
-        let mutable costSoFar = Dictionary<Point2D * Dir, int>()
-        costSoFar[(start, sdir)] <- sp
+        q.Enqueue(
+            { cp = start
+              cd = R
+              score = sp
+              path = [ start ] },
+            0
+        )
 
         let mutable found = false
+        let mutable maxScore = None
 
-        let mutable result = (0, [])
+        while q.Count > 0 do
+            let c = q.Dequeue()
+            let cp, cd, score, path = c.Unpack
 
-        while not found && q.Count > 0 do
-            let (cp, cd), junctions = q.Dequeue()
+            if (maxScore.IsSome && score > maxScore.Value) then
+                ()
+            else
+                if cp = target then
+                    maxScore <- Some(score)
 
-            if cp = target then
-                let mutable c = Some((cp, cd))
-                let mutable count = 0
+                    for p in path do
+                        visitedBestPath[p] <- 1
 
-                visited[start] <- 1
-                visited[target] <- 1
+                    found <- true
 
-                result <- (costSoFar[(cp, cd)], junctions)
+                if (not (visited.ContainsKey(cp, cd)) || (score <= visited[(cp, cd)])) then
+                    visited[(cp, cd)] <- score
 
-                while c.IsSome do
-                    count <- count + 1
-                    let cp, cd = c.Value
-                    c <- cameFrom[(cp, cd)]
-                    visited[cp] <- 1
+                    let neighbours =
+                        neighbourOffsets
+                        |> List.map (fun offs -> (offs + cp, d2[offs]))
+                        |> List.where (fun (_, d) -> canMove cd d)
 
-                found <- true
+                    let mutable choices =
+                        neighbours
+                        |> List.where (fun (np, _) -> (np.y >= 0 && np.y < rows && np.x >= 0 && np.x < cols && (gr[np.y][np.x] <> '#')))
+                        |> List.map (fun (np, nd) ->
+                            let newScore = score + (if nd = cd then 1 else 1001)
+                            (np, nd, newScore))
 
-                //pr ()
+                    choices
+                    |> List.iter (fun (np, nd, ns) ->
+                        q.Enqueue(
+                            { cp = np
+                              cd = nd
+                              score = ns
+                              path = path @ [ np ] },
+                            score
+                        ))
 
-            let neighbours =
-                noffs
-                |> Seq.map (fun offs -> (offs + cp, d2[offs]))
-                |> Seq.where (fun (_, d) -> canMove cd d)
+        maxScore
 
-            let mutable choices = []
-
-            for np, nd in neighbours do
-                let newCost = costSoFar[(cp, cd)] + (if nd = cd then 1 else 1001)
-
-                if
-                    ((mv.IsNone || newCost <= mv.Value)
-                     && (not (costSoFar.ContainsKey(np, nd)) || newCost < costSoFar[(np, nd)])
-                     && np.y >= 0
-                     && np.y < rows
-                     && np.x >= 0
-                     && np.x < cols
-                     && (gr[np.y][np.x] <> '#'))
-                then
-                    choices <- choices @ [ (np, nd, newCost) ]
-
-            let lj =
-                if (choices |> Seq.length > 1) then
-                    let ch = choices |> List.map (fun (np, nd, nc) -> ((np, nc), nd))
-                    junctions @ ch
-                else
-                    junctions
-
-            for np, nd, nc in choices do
-                costSoFar[(np, nd)] <- nc
-                let priority = nc
-                q.Enqueue(((np, nd), lj), priority)
-                cameFrom[(np, nd)] <- Some(cp, cd)
-
-
-        if (fst result) <> 0 then Some(result) else None
-
-    let r = (fp S E 0 R None)
-    let junctions = snd r.Value
-    let mv = fst r.Value
-
-
-
-    for (j, sp), d in junctions |> Seq.skip 1 do
-        fp j E sp d (Some(mv)) |> ignore
-
-
-    (*
-    printfn $"%A{visited.Count}"
-    printfn $"%A{junctions}"
-    *)
-
-    printfn $"%A{visited.Count}"
+    let bestPathScore = fp S E 0
+    printfn $"%A{(bestPathScore, visitedBestPath.Count)}"
     0
-
-
-//io.submitAnswer 1 a1
-//io.submitAnswer 2 a2
